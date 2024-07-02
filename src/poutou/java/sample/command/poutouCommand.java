d2cb39b4e3e0b799d067cfd2d7564394b7e6b894
Alban_Dalmard <alban.dalmard@etu.minesparis.psl.eu>
2024-07-02 10:35:43 +0900

    addCustomer

---
+++ src/poutou/java/sample/command/poutouCommand.java
@@ -0,0 +1,34 @@
+package poutou.command;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+
+@Command(
+    name = "bin/poutou",
+    description = "poutou's application for Microservice Transaction",
+    subcommands = {
+      LoadInitialDataCommand.class,
+      GetCustomerInfoCommand.class,
+    })
+public class poutouCommand implements Runnable {
+
+  @Option(
+      names = {"-h", "--help"},
+      usageHelp = true,
+      description = "Displays this help message and quits.",
+      defaultValue = "true")
+  private Boolean showHelp;
+
+  @Override
+  public void run() {
+    if (showHelp) {
+      CommandLine.usage(this, System.out);
+    }
+  }
+
+  public static void main(String[] args) {
+    new CommandLine(new poutouCommand()).execute(args);
+  }
+}
